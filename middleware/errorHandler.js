e262ef3457c88d52fc6849fad91294ef059882c8
Richard <richardosunmu@gmail.com>
2023-04-12 15:59:50 +0100

    Minor changes
    Better if the stack trace is in the logs than in the response sent back to the client

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,8 @@
 export const errorHandler = (err, req, res, next) => {
     if (err)
     {
-        res.send({Title: "Error", Message: err.message, stackTrace: err.stack})
+        console.log({Title: "Error", Message: err.message, stackTrace: err.stack});
+        res.send({Title: "Error", Message: err.message});
     }
     else{
         console.log("No Error, All Good");
